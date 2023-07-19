418e666e76afc22c95559db0ddc769adb63fe435
Sergio Rodriguez <sergiordg00@gmail.com>
2023-07-19 17:25:38 +0200

    Everything ready

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google';
 
-import './globals.css';
+import "@/assets/css/reset.css";
+import "@/assets/css/globals.css";
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -9,10 +10,12 @@ export const metadata = {
   description: 'Experience next-level communication with our web application, designed to reinvent the way you connect. Seamlessly chat with individuals or in groups, harnessing the power of advanced AI to make your conversations more efficient and insightful. A user-friendly interface meets cutting-edge technology for a superior chatting experience.',
 };
 
-export default function RootLayout({ children }) {
+export default async function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+      </body>
     </html>
   );
 }
