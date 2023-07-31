3179730db3c53d6d923232456eb8cab798c6eeff
Sergio Rodriguez <sergiordg00@gmail.com>
2023-07-31 22:38:36 +0200

    Main sidebar completed

--- shared-components/ui/Sidebar.jsx
+++ shared-components/ui/Sidebar.jsx
@@ -1,9 +1,12 @@
 import DesktopSidebar from "./DesktopSidebar";
+import MobileFooter from "./MobileFooter";
 
 export default async function Sidebar({ children }) {
   return (
     <div className="h-full">
       <DesktopSidebar />
+
+      <MobileFooter />
       
       <main className="h-full lg:pl-20">
         {children}
