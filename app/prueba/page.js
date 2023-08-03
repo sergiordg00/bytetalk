6688a889f9704b78ad1e472b59676c1ba8818c2c
Sergio Rodriguez <sergiordg00@gmail.com>
2023-08-03 11:04:08 +0200

    App modal with mount and unmount animations

--- app/prueba/page.js
+++ app/prueba/page.js
@@ -3,7 +3,7 @@
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
-import Modal from "@/shared-components/modals/Modal";
+import AppModal from "@/shared-components/modals/AppModal";
 
 export default function Prueba() {
   const [showModal, setShowModal] = useState(false);
@@ -11,15 +11,14 @@ export default function Prueba() {
 
   return (
     <>
-      <Modal 
+      <AppModal
         isOpen={showModal} 
         onClose={() => setShowModal(false)}
-        className="w-full max-w-[500px] rounded-lg bg-white p-4"
       >
         <h1>Test de modal</h1>
 
         <button onClick={() => router.push("/users")} className="cursor-pointer bg-sky-500 p-2 transition-all active:translate-y-1">Ir a inicio</button>
-      </Modal>
+      </AppModal>
 
       <button onClick={() => setShowModal(true)} className="cursor-pointer bg-sky-500 p-2 transition-all active:translate-y-1">Abrir modal</button>
 
---
+++ shared-components/modals/AppModal.jsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { IoClose } from "react-icons/io5";
+
+import Modal from "./Modal";
+
+export default function AppModal({ isOpen, onClose, children }) {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      className="w-full rounded-lg bg-white p-4 text-left sm:max-w-lg sm:p-6"
+    >
+      <button 
+        type="button" 
+        className="absolute right-2 top-2 rounded-md bg-white text-gray-400 hover:text-gray-500 sm:right-4 sm:top-4" 
+        onClick={onClose}
+      >
+        <span className="sr-only">
+            Close
+        </span>
+
+        <IoClose className="h-auto w-6"/>
+      </button>
+
+      {children}
+    </Modal>
+  );
+}
