e1ead29fb51a73928b4c1e4030a76967d5771348
Sergio Rodriguez <sergiordg00@gmail.com>
2023-09-06 12:33:42 +0200

    feat: share image

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 
+import share from "@/assets/img/share.jpg";
 import AuthContext from "@/context/AuthContext";
 import { ThemeProvider } from "@/context/ThemeContext";
 import ToasterContext from "@/context/ToasterContext";
@@ -12,6 +13,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: 'ByteTalk - Chat with your friends',
   description: 'Experience next-level communication with our web application, designed to reinvent the way you connect. Seamlessly chat with individuals or in groups, harnessing the power of advanced AI to make your conversations more efficient and insightful. A user-friendly interface meets cutting-edge technology for a superior chatting experience.',
+  image: share,
 };
 
 export default async function RootLayout({ children }) {
